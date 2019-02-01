95df9c735cb29e8a8efa53bdd7251ed156356b7b
Seb Cooper <2315570c@student.gla.ac.uk>
2019-02-01 11:04:44 +0000

    added about page (page 26)

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+#create a view for /about
+def about (request):
+	#create a message
+	message_dict = {'boldmessage': "Rango says here is the about page"}
+	#return a rendered response
+	return render(request, 'rango/about.html', context = message_dict)
+	
+
 def index(request):
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
@@ -10,6 +18,7 @@ def index(request):
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
     return render(request, 'rango/index.html', context=context_dict)
+
 
 
 
